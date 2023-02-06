b2c98e857651ddaa1d5c0fc0dd31a0c169418652
Keenan-Faure <keenan.asterisk@gmail.com>
2023-02-06 09:04:54 +0200

    added FileLog

---
+++ src/FileLog.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keenan\Logger;
+
+use Keenan\Logger\includes\Logs;
+
+class FileLog extends Logs
+{
+    public static function fileLog(string $level, string $message): void
+    {
+        $logContent = 
+        [
+            "name" => "Log",
+            "useJSONFormatter" => false,
+            "fileHandler" => true,
+            "useStreamHandle" => true,
+            "level" => $level
+        ];
+        $consoleLog = new Logs($logContent);
+        $consoleLog->LogConsole($message);
+    }
+}
+
+?>
