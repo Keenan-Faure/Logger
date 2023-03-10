48a46aac5f4004ccc147532a95afe896f1e7279e
Keenan Faure <97687673+Keenan-Faure@users.noreply.github.com>
2023-03-10 15:47:43 +0200

    session fix

--- src/__Init.php
+++ src/__Init.php
@@ -4,26 +4,24 @@
     use Keenan\Logger\includes\Utils;
 
     include_once(Utils::getInit());
-    if(!isset($_SESSION) || !isset($_SESSION['config']))
+
+    try
     {
-        try
+        $url = Utils::createFolderMap();
+        $filePath = $url . '/vendor/keenan/logger/config/config.php';
+        if(file_exists($filePath))
         {
-            $url = Utils::createFolderMap();
-            $filePath = $url . '/vendor/keenan/logger/config/config.php';
-            if(file_exists($filePath))
-            {
-                $_config = include($filePath);
-                $_SESSION['config'] = $_config;
-            }
-            else
-            {
-                $_config = include($url . '/config/config.php');
-                $_SESSION['config'] = $_config;
-            }
+            $_config = include($filePath);
+            $_SESSION['config'] = $_config;
         }
-        catch(\Exception $error)
+        else
         {
-            FileLog::fileLog('warning', $error->getMessage());
+            $_config = include($url . '/config/config.php');
+            $_SESSION['config'] = $_config;
         }
+    }
+    catch(\Exception $error)
+    {
+        FileLog::fileLog('warning', $error->getMessage());
     }
 ?>
